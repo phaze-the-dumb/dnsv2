8e9b0ee7d5d68d71ee4eff81c9075ed7bc808c28
wiresboy-exe <liamcbone@gmail.com>
2022-06-26 15:48:31 +0100

    oops

--- analytics/html.js
+++ analytics/html.js
@@ -3,7 +3,7 @@ let startLoadTime = Date.now();
 window.addEventListener('load', () => {
     let dt = Date.now() - startLoadTime;
 
-    fetch('http://192.168.11.13:100/api/v1/analytics/page?url='+window.location.href+'&loadTime='+dt, { method: 'POST' }).then(data => data.json()).then(data => {
+    fetch('https://dns.phazed.xyz/api/v1/analytics/page?url='+window.location.href+'&loadTime='+dt, { method: 'POST' }).then(data => data.json()).then(data => {
         console.log('[VOXEL]', 'Registered page');
     })
 });
